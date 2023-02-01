4524c942249b53a33560b922fc8b494f2a1ac7ea
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2023-02-01 14:29:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+function App() {
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Data, setData] = useState([]);
+
+  function addData() {
+    setData([...Data, { Name, Email }]);
+    setName("");
+    setEmail("");
+  }
+  function delitem(index){
+    let arr = Data
+    arr.splice(index,1);
+    setData([...arr]);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="form">
+        <Stack direction="row" spacing={2}>
+          <TextField
+            value={Name}
+            onChange={(e) => setName(e.target.value)}
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <Button onClick={addData} variant="contained" color="success">
+            <AddIcon /> ADD
+          </Button>
+        </Stack>
+      </div>
+
+      <div className="data">
+        <div className="data_val">
+          <h3>Name</h3>
+          <h3>Email</h3>
+          <h3>Remove</h3>
+        </div>
+        {Data.map((element, index) => {
+          return (
+            <div key={index} className="data_val">
+              <h3>{element.Name}</h3>
+              <h3>{element.Email}</h3>
+              <Button onClick={ () => delitem(index)} variant="contained" color="error">
+                <DeleteIcon />
+              </Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
